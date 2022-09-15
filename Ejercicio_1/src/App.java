7431ce60947912779e016aaead9d61071cb78d1a
Diego Pacori <dpacoria@unsa.edu.pe>
2022-09-14 23:56:05 -0500

    Cambios minimos

--- Ejercicio_1/src/App.java
+++ Ejercicio_1/src/App.java
@@ -1,18 +1,23 @@
 public class App {
     public static void main(String[] args){
+        //1era Parte
         System.out.println(Suma(1,5,9));
         System.out.println(Suma(10,50,90));
         System.out.println(Suma(1,2,3));
+
+        //2da Parte
         Coche miCoche = new Coche();
         miCoche.aumentarPuertas();
         System.out.println(miCoche);
     }
+
     public static int Suma(int a, int b, int c) {
         return a+b+c;
     }
+
 }
 class Coche {
-    private int numPuertas = 0;
+    private int numPuertas = 4;
 
     public void aumentarPuertas() {
         this.numPuertas++;
